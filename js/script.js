7818d1b4776779a59899b0401c97c2d81bc67a2d
SpiceMerchant1337 <88102936+SpiceMerchant1337@users.noreply.github.com>
2022-12-12 10:04:36 +0300

    Revise coding challenge 4

--- js/script.js
+++ js/script.js
@@ -1001,7 +1001,7 @@ while (dice !== 6) {
 } */
 
 // CODING CHALLENGE 4 - JS Fundamentals Part 2
-let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+/* let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 let tips = [];
 let totals = [];
 
@@ -1015,4 +1015,31 @@ for (let i = 0; i < bills.length; i++) {
 
 console.log(`bills = ${bills}`);
 console.log(`tips = ${tips}`);
-console.log(`totals = ${totals}`);
+console.log(`totals = ${totals}`); */
+
+// redoing the coding challenge
+/* let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+let calcTip = (bill) => (bill <= 300 ? bill * 0.15 : bill * 0.2);
+
+for (let i = 0; i < bills.length; i++) {
+  let tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+
+console.log(bills);
+console.log(tips);
+console.log(totals); */
+
+/* const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals)); */
